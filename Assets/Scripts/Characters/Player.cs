f8d351e85fdf9ecb5a175b3e3de16659f6a4a9cc
JamesNemeth <jamesnemeth1999@gmail.com>
2019-08-30 11:40:45 +1000

    First Push
    First Version with movement, items and animations

---
+++ Assets/Scripts/Characters/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController2D))]
+public class Player : MonoBehaviour
+{
+    //member variables
+    public float jumpHeight = 5f;   //how high the character jumps
+    public float climbSpeed = 10f;  // how fast the character climbs
+    public float moveSpeed = 10f;  //how fast the character moves
+
+    private CharacterController2D controller;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // gather components at the start of the game to save processing
+        controller = GetComponent<CharacterController2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*
+        * --- Unity Tip
+        * Input.GetAxis 
+        * Input.GetAxisRaw
+        */
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        bool isJumping = Input.GetButtonDown("Jump");
+
+        if (isJumping)
+        {
+            controller.Jump(jumpHeight);
+        }
+
+        controller.Climb(vertical * climbSpeed);
+
+         // move controller horizontally
+        controller.Move(horizontal * moveSpeed);
+    }
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "Item") 
+        {
+            //Detect hitiing item
+            //Add 1 score
+            GameManager.Instance.Addscore(1);
+            //Play Chime sound
+            Destroy(col.gameObject);
+        }
+
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    #region Singleton
+    public static GameManager Instance = null;
+    private void Awake()
+    {
+        Instance = this;
+    }
+    #endregion
+
+    public int score = 0; //ScoreKeeping
+
+    public void Addscore(int scoreToAdd)
+    {
+        //Increase score Value by incoming score
+        score += scoreToAdd;
+        //Update UI here
+        UIManager.Instance.UpdateScore(score);
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void NextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void PrevLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
